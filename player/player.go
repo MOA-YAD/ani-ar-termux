b1feabb6a7de778c9ef704471f2a0fc77e657cf0
MOAYAD <moayad102090@gmail.com>
2024-12-05 19:43:40 +0300

    Update player.go

--- player/player.go
+++ player/player.go
@@ -1,56 +1,15 @@
 package player
 
 import (
-	"errors"
-	"os/exec"
+        "os/exec"
+        "fmt"
 )
 
-type Player struct {
-	bin     string
-	execute func(url, title string) *exec.Cmd
-}
-
-var players []Player = []Player{
-	{
-		bin: "mpv",
-		execute: func(u, t string) *exec.Cmd {
-			cmd := exec.Command(
-				"mpv",
-				"--title="+t,
-				u)
-			return cmd
-		},
-	},
-	{
-		bin: "vlc",
-		execute: func(u, t string) *exec.Cmd {
-			cmd := exec.Command(
-				"vlc",
-				"--play-and-exit",
-				"--meta-title="+t,
-				u)
-			return cmd
-		},
-	},
-}
-
-func RunVideo(url, title string) (*exec.Cmd, error) {
-	for _, player := range players {
-		exist := commandExists(player.bin)
-		if exist {
-			cmd := player.execute(url, title)
-			err := cmd.Start()
-			if err != nil {
-				return nil, err
-			}
-			//		log.Printf("video played with PID %d\n", cmd.Process.Pid)
-			return cmd, nil
-		}
-	}
-	return nil, errors.New("you don't any players to play the episode try installing vlc or mpv")
-}
-
-func commandExists(cmd string) bool {
-	_, err := exec.LookPath(cmd)
-	return err == nil
+func RunVideo(url, title string) (string, error) {
+        video_title := fmt.Sprintf("Title:%0s\n", title)
+        fmt.Println(video_title)
+        cmd := exec.Command("termux-open-url", url)
+        cmd.Run()
+        return video_title, nil
+        return url, nil
 }
